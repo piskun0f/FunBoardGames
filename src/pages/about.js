1fdd161943986a3dec3db4685245cec85b795821
Андрей Пискунов <adpiskunov@miem.hse.ru>
2021-09-16 00:18:49 +0300

    ✨ Add links & CSS

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle='About'>
+            <p>This page is about me. I am Andrew Piskunov.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
